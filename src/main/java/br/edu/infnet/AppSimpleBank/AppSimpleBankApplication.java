7f290388f8b68feede39b100dd28646371c08d03
Victor Vianna <vitorvianna27@gmail.com>
2023-11-15 12:42:44 -0300

    :sparkles:(feat): First Commit

---
+++ src/main/java/br/edu/infnet/AppSimpleBank/AppSimpleBankApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.AppSimpleBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSimpleBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppSimpleBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/infnet/AppSimpleBank/AppSimpleBankApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.AppSimpleBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSimpleBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
