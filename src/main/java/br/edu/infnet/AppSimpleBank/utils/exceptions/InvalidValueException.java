9a4d56a0f1caf3e4702b5209eb8e37e524f40b0c
Victor Vianna <vitorvianna27@gmail.com>
2023-11-15 13:42:59 -0300

    :pencil2:fix: Add Exceptions

---
+++ src/main/java/br/edu/infnet/AppSimpleBank/utils/exceptions/InvalidValueException.java
@@ -0,0 +1,7 @@
+package br.edu.infnet.AppSimpleBank.utils.exceptions;
+
+public class InvalidValueException extends Exception {
+    public InvalidValueException(String msg) {
+        super(msg);
+    }
+}
---
+++ src/main/java/br/edu/infnet/AppSimpleBank/utils/exceptions/NullOrEmptyException.java
@@ -0,0 +1,7 @@
+package br.edu.infnet.AppSimpleBank.utils.exceptions;
+
+public class NullOrEmptyException extends Exception {
+    public NullOrEmptyException(String msg) {
+        super(msg);
+    }
+}
