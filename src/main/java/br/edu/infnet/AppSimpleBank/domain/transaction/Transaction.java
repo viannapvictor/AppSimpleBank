36e8b1f75ac357526d8991c48e956da2d6ca74da
Victor Vianna <vitorvianna27@gmail.com>
2023-11-15 13:16:28 -0300

    :sparkles:feat: Getters and Setters

--- src/main/java/br/edu/infnet/AppSimpleBank/domain/transaction/Transaction.java
+++ src/main/java/br/edu/infnet/AppSimpleBank/domain/transaction/Transaction.java
@@ -11,4 +11,52 @@ public class Transaction {
     private User receiver;
     private String password;
     private LocalDateTime transactionDate;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public User getSender() {
+        return sender;
+    }
+
+    public void setSender(User sender) {
+        this.sender = sender;
+    }
+
+    public User getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(User receiver) {
+        this.receiver = receiver;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDateTime getTransactionDate() {
+        return transactionDate;
+    }
+
+    public void setTransactionDate(LocalDateTime transactionDate) {
+        this.transactionDate = transactionDate;
+    }
 }
--- src/main/java/br/edu/infnet/AppSimpleBank/domain/user/User.java
+++ src/main/java/br/edu/infnet/AppSimpleBank/domain/user/User.java
@@ -10,4 +10,68 @@ public class User {
     private String password;
     private double balance;
     private UserTypeEnum userType;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastNam() {
+        return lastNam;
+    }
+
+    public void setLastNam(String lastNam) {
+        this.lastNam = lastNam;
+    }
+
+    public String getDocument() {
+        return document;
+    }
+
+    public void setDocument(String document) {
+        this.document = document;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public UserTypeEnum getUserType() {
+        return userType;
+    }
+
+    public void setUserType(UserTypeEnum userType) {
+        this.userType = userType;
+    }
 }
