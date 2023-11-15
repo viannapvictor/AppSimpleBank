eb1e74bf95183653e98a94be409a147e0f1f284b
Victor Vianna <vitorvianna27@gmail.com>
2023-11-15 12:55:57 -0300

    :sparkles:feat: User Domain

---
+++ src/main/java/br/edu/infnet/AppSimpleBank/domain/user/User.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.AppSimpleBank.domain.user;
+
+public class User {
+
+    private int id;
+    private String firstName;
+    private String lastNam;
+    private String document;
+    private String email;
+    private String password;
+    private double balance;
+    private UserTypeEnum userType;
+}
---
+++ src/main/java/br/edu/infnet/AppSimpleBank/domain/user/UserTypeEnum.java
@@ -0,0 +1,20 @@
+package br.edu.infnet.AppSimpleBank.domain.user;
+
+public enum UserTypeEnum {
+    Client ("Cliente"),
+    MERCHANT ("Lojista");
+
+    private final String userType;
+    UserTypeEnum(String userType) {
+        this.userType = userType;
+    }
+
+    public String getUserType () {
+        return userType;
+    }
+
+    @Override
+    public String toString() {
+        return getUserType();
+    }
+}
